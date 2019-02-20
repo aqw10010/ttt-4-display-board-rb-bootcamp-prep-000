f0d7525a67123cf0069d6919e40fc0f1878432c1
Austin Williams <austinq.williams@gmail.com>
2019-02-20 18:47:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,14 +2,3 @@
 # out the current state.
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
  
-def display_board(board)
-  
-  puts "the currrent state of tic tac toe #{board}."
-  
-puts " #{board[0]} |  #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]}  |  #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]}  |  #{board[7]} | #{board[8]} " 
-  
-end 
